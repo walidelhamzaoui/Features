a88d46a7ea56cf7b44e9b024953e2febc2ccf074
ELhamzaouiWalid <walidelhamzoui@gmail.com>
2023-12-20 17:21:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function button() {
+    var oki1 = document.getElementById("oki1");
+    var para = document.getElementById("para");
+
+    // Change the color of element with id "oki1" to red
+    // oki1.style.color = "red";
+
+    // Toggle the display of element with id "sat"
+    if (para.style.display === "none") {
+        para.style.display = "block";
+        
+    } else {
+        para.style.display = "none";
+    }
+}
+function button1() {
+    var oki1 = document.getElementById("oki1");
+    var para = document.getElementById("para1");
+
+    // Change the color of element with id "oki1" to red
+    // oki1.style.color = "red";
+
+    // Toggle the display of element with id "sat"
+    if (para.style.display === "none") {
+        para.style.display = "block";
+        
+    } else {
+        para.style.display = "none";
+    }
+}
+function button2() {
+    var oki1 = document.getElementById("oki1");
+    var para = document.getElementById("para2");
+
+    // Change the color of element with id "oki1" to red
+    // oki1.style.color = "red";
+
+    // Toggle the display of element with id "sat"
+    if (para.style.display === "none") {
+        para.style.display = "block";
+        
+    } else {
+        para.style.display = "none";
+    }
+}
